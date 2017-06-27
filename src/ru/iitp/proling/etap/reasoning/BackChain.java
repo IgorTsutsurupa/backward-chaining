45519c65a60eddc37e650112187ffac7153b91ce
Igor Tsutsurupa <igor.tsutsurupa97@gmail.com>
2017-06-27 23:58:00 +0200

    rewriting BackChain from scratch

--- src/ru/iitp/proling/etap/reasoning/BackChain.java
+++ src/ru/iitp/proling/etap/reasoning/BackChain.java
@@ -11,43 +11,20 @@ public class BackChain {
 
 	public static List<Conjunction> getRewritings(Conjunction query, RuleStorage rules) {
 		List<Conjunction> result = new ArrayList<Conjunction>();
-		Queue<Atom> queue = new LinkedList<Atom>();
+		Queue<Conjunction> mainQueue = new LinkedList<Conjunction>();
 
-		// положить все атомы, входящие в query, в очередь, и работать в
-		// последствии
-		// с каждым элементом в очереди
-		for (Atom atom : query.getAtoms()) {
-			queue.add(atom);
-		}
-		result.add(new Conjunction(queue.peek()));
-		while (!queue.isEmpty()) {
-			Atom a = queue.poll();
-			Conjunction current = new Conjunction(a);
-
-			// ищем в списке правила, в которые текущая конъюнкция (состоящая из
-			// одного атома)
-			// входит как правая часть
-			List<Rule> list = rules.searchRules(current);
-			for (Rule r : list) {
-
-				// проверим, не входят ли конъюкты добавляемой конъюнкции
-				// в какую-либо из уже добавленных конъюнкций
-				// если да -- то не добавлять (во избежание зацикливания)
-				boolean include = false;
-				Conjunction t = r.getBody();
-				for (Conjunction c : result) {
-					if (c.check(t)) {
-						include = true;
-						break;
-					}
-				}
-				if (!include) {
-					result.add(t);
-
-					// Добавим в очередь конъюнкты из левой части нужного
-					// правила
-					queue.addAll(t.getAtoms());
-				}
+		result.add(query);
+		mainQueue.add(query);
+		while (!mainQueue.isEmpty()) {
+			Conjunction entry = mainQueue.poll();
+			Queue<Conjunction> entryAtoms = new LinkedList<Conjunction>();
+			for (Atom currentAtom : entry.getAtoms()) {
+				entryAtoms.add(new Conjunction(currentAtom));
+			}
+			while (!entryAtoms.isEmpty()) {
+				Conjunction atom = entryAtoms.poll();
+				List<Rule> wantedRules = rules.searchRules(atom);
+								
 			}
 		}
 		return result;
